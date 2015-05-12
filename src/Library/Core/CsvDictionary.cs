c7f7645786d158458a4d9cf63c17bbe4f0510be2
Robert Giesecke <robert.giesecke@gmail.com>
2015-05-12 16:20:04 +0200

    added DynamicView to CsvDictionary

--- src/Library/Core/CsvDictionary.cs
+++ src/Library/Core/CsvDictionary.cs
@@ -4,23 +4,43 @@ namespace RGiesecke.PlainCsv.Core
 {
   public class CsvDictionary<TValue> : OrderedDictionary<string, TValue>
   {
+#if DynamicObject
+private readonly DynamicDictionaryView<TValue> _DynamicView;
+
+    public dynamic DynamicView
+    {
+      get { return _DynamicView; }
+    }
+#endif
     public CsvDictionary()
     {
+#if DynamicObject
+      _DynamicView = new DynamicDictionaryView<TValue>(this);
+#endif
     }
 
     public CsvDictionary(IEqualityComparer<string> keyComparer)
       : base(keyComparer)
     {
+#if DynamicObject
+      _DynamicView = new DynamicDictionaryView<TValue>(this);
+#endif
     }
 
     public CsvDictionary(IEnumerable<string> keys, IDictionary<string, TValue> dictionary)
       : base(keys, dictionary)
     {
+#if DynamicObject
+      _DynamicView = new DynamicDictionaryView<TValue>(this);
+#endif
     }
 
     public CsvDictionary(IEnumerable<string> keys, IDictionary<string, TValue> dictionary, IEqualityComparer<string> keyComparer)
       : base(keys, dictionary, keyComparer)
     {
+#if DynamicObject
+      _DynamicView = new DynamicDictionaryView<TValue>(this);
+#endif
     }
   }
 }
